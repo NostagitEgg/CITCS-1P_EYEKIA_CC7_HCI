db419de9adbfd5dffdc2936c4926e4514b9ef7f8
NostagitEgg <93234548+NostagitEgg@users.noreply.github.com>
2024-11-28 11:36:45 +0800

    Subtitles

--- EYEKIA Simulation/Assets/Scripts/ProximityTrigger.cs
+++ EYEKIA Simulation/Assets/Scripts/ProximityTrigger.cs
@@ -1,36 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ProximityTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI subtitles;
+    string subText;
+
+    bool isTalking; //To check if someone near is talking
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        //If the object in the trigger collider of the player is named...do this
+        string name = other.name;
+        switch (name)
+        {
+            case "StartMan":
+                isTalking = true;
+                subText = "Oh no, whatever happened there! Was it some accident?";
+                StartCoroutine(WordForWord());
+                break;
+
+            case "CrimeWoman":
+                isTalking = true;
+                subText = "I was driving and suddenly some guy appeared right in front of me. I had to swerve!";
+                StartCoroutine(WordForWord());
+                break;
+
+            case "Man":
+                isTalking = true;
+                subText = "What happned just now? I saw the indicator on my Eyekia glasses.";
+                StartCoroutine(WordForWord());
+                break;
+
+            case "LorryCargo":
+            case "Police":
+            case "Minivan":
+                Debug.Log("Engine Noises");
+                break;
+            case "Ambulance":
+                Debug.Log("Sirens");
+                break;
+
+            default:
+                subText = "";
+                subtitles.text = subText;
+                isTalking = false;
+                break;
+        }  
+    }
+
+    //To have no text when out of radius
+    private void OnTriggerExit(Collider other)
+    {
+        subText = "";
+        subtitles.text = subText;
+        isTalking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //To check if still talking or not
+        if (subtitles.text.Length <= subText.Length)
+        {
+            isTalking = true;
+        }
+        else
+        {
+            isTalking = false;
+        }
+
     }
 
-    private void OnTriggerStay(Collider other)
+    IEnumerator WordForWord()
     {
-        //Text Test
-        string name = other.name;
-        switch (name)
+        if (isTalking)
         {
-            case "StartMan":
-                Debug.Log("What a beautiful day");
-                break;
-            case "LorryCargo":
-                Debug.Log("Engine Noises");
-                break;
-            case "Police":
-                Debug.Log("Engine Noises");
-                break;
-        }  
+            //To individually show words like live captions
+            foreach (string word in subText.Split(' '))
+            {
+                yield return new WaitForSeconds(0.2f); //As some words were not being picked up at the start
+                subtitles.text += word + " ";
+                float randomTime = Random.Range(0.1f, 0.2f);
+                yield return new WaitForSeconds(randomTime); //pauses between words to mimic live captioning
+            }
+        }
     }
+
+
 }
+
